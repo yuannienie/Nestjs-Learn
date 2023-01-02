d377963a08a9152b1b7f232f86ec65af60c2326f
jonie <jonie@tencent.com>
2023-01-02 22:31:20 +0800

    feat: class providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,14 +7,24 @@ import { CoffeeService } from './coffee.service';
 import { Module } from '@nestjs/common';
 import { Event } from 'src/events/entities/event.entity';
 
-class MockCoffeeService {}
+// class MockCoffeeService {}
+class ConfigService {}
+class DevelopmentService {}
+class ProductionService {}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeeController],
-  // providers: [{ provide: CoffeeService, useValue: new MockCoffeeService() }],
   providers: [
     CoffeeService,
+    // [{ provide: CoffeeService, useValue: new MockCoffeeService() }], // useValue
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentService
+          : ProductionService,
+    },
     { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
   ],
   exports: [CoffeeService],
