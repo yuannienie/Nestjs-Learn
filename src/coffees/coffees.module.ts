4417b26388ff51e5ee5901179b5c8f90b721741b
jonie <jonie@tencent.com>
2023-01-02 22:42:25 +0800

    feat: factory providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,28 +4,39 @@ import { Coffee } from './entities/coffee.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeController } from './coffee.controller';
 import { CoffeeService } from './coffee.service';
-import { Module } from '@nestjs/common';
+import { Inject, Module, Injectable } from '@nestjs/common';
 import { Event } from 'src/events/entities/event.entity';
 
 // class MockCoffeeService {}
-class ConfigService {}
-class DevelopmentService {}
-class ProductionService {}
-
+// class ConfigService {}
+// class DevelopmentService {}
+// class ProductionService {}
+@Injectable()
+class CoffeeBrandsFactory {
+  create() {
+    return ['buddy brew', 'nescafe', 'coco'];
+  }
+}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeeController],
   providers: [
     CoffeeService,
     // [{ provide: CoffeeService, useValue: new MockCoffeeService() }], // useValue
+    // {
+    //   provide: ConfigService,
+    //   useClass:
+    //     process.env.NODE_ENV === 'development'
+    //       ? DevelopmentService
+    //       : ProductionService,
+    // },
+    CoffeeBrandsFactory,
     {
-      provide: ConfigService,
-      useClass:
-        process.env.NODE_ENV === 'development'
-          ? DevelopmentService
-          : ProductionService,
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
     },
-    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
   ],
   exports: [CoffeeService],
 })
